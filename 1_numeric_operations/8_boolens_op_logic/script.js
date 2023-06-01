055071eefa4fcec268364e7420b183455f78a26a
Vida Freitas <vidavalessa65@gmail.com>
2023-06-01 17:21:14 -0300

    feat: add 1 more lesson

---
+++ 1_numeric_operations/8_boolens_op_logic/script.js
@@ -0,0 +1,6 @@
+console.log(5>3&&3==2);
+console.log(5>3||3==1);
+console.log(3==3&& "vida"== "vida");
+console.log("sol" == "lua" || false);
+console.log(!(true&&true));
+console.log(false||false);
