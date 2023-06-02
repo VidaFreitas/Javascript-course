99d87c1798352ed83c9a1c036fa5f1acb484c98c
Vida Freitas <vidavalessa65@gmail.com>
2023-06-02 16:20:38 -0300

    feat: add the first lesson

---
+++ 2_program_structure/1_variables/script.js
@@ -0,0 +1,19 @@
+let nome = "Vida";
+console.log(nome);
+console.log('O meu nome é ${nome}');
+
+let morango = 10;
+let laranjas = 12;
+console.log(morango * laranjas);
+
+nome = "Lua";
+console.log(nome);
+
+laranjas = 30;
+console.log(laranjas);
+
+laranjas = "Laranjas";
+console.log(laranjas);
+
+let um = 5, dois = 8, tres = 10;
+console.log(um - dois + tres);
