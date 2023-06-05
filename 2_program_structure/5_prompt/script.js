811c42892e26409eb4a51e9e589886f90d8b8c37
Vida Freitas <vidavalessa65@gmail.com>
2023-06-05 16:53:19 -0300

    feat; add 1 lesson

---
+++ 2_program_structure/5_prompt/script.js
@@ -0,0 +1,8 @@
+let idade = prompt("Qual é sua idade?");
+console.log(idade);
+
+let nome = prompt("Qual é o seu nome?");
+console.log(nome);
+
+let cidade = prompt("Onde voce reside?");
+console.log(cidade);
