d48d4dd3fd8c4c188baf75e05020c9279e5b120b
Vida Freitas <vidavalessa65@gmail.com>
2023-06-05 17:29:51 -0300

    feat; add 1 more lesson

---
+++ 2_program_structure/7_math/script.js
@@ -0,0 +1,11 @@
+let bigger = Math.max(6,12,28,55,8);
+console.log(bigger);
+
+let smaller = Math.min(40,18,21,8,5);
+console.log(smaller);
+
+let round = Math.round(7.84255);
+console.log(round);
+
+let roundup = Math.ceil(5.41235);
+console.log(roundup);
