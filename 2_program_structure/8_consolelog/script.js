35112ceadc2461e2e57aa8099592eb4ba4076d18
Vida Freitas <vidavalessa65@gmail.com>
2023-06-05 17:44:35 -0300

    feat; add 1 more lesson

---
+++ 2_program_structure/8_consolelog/script.js
@@ -0,0 +1,9 @@
+let age = 27;
+let nome = "Vida";
+let lastname = "Freitas";
+
+console.log(age);
+console.log(nome);
+console.log(lastname);
+
+console.log(`O meu nome é ${nome} ${lastname}, e tenho ${age}`);
