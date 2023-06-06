fd28e61db44ec31c707a323a58028a76521a9453
Vida Freitas <vidavalessa65@gmail.com>
2023-06-06 17:44:29 -0300

    feat. add 1 more lesson

---
+++ 2_program_structure/15_break/script.js
@@ -0,0 +1,14 @@
+ let nome = "Vida";
+
+for(let i = 0; i < 10, i = i + 1;) {
+
+     if (i == 3) {
+        nome = "Lara";
+    }
+
+    if (i == 5 && nome == "Lara") {
+        console.log("O nome é João, pode parar");
+        break;
+    }
+        console.log(i);
+}
