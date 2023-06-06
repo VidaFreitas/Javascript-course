ba661ca112806397ae315eaa3a80b6d9f55dd76e
Vida Freitas <vidavalessa65@gmail.com>
2023-06-06 16:34:42 -0300

    feat; add 1 lesson

---
+++ 2_program_structure/9_if/script.js
@@ -0,0 +1,21 @@
+let idade = 19;
+
+if(idade ==19) {
+    console.log("A idade é = 19");
+}
+
+if(idade > 25) {
+    console.log("A idade é maipr qie 25");
+}
+
+let nome = "Vida";
+
+if(nome == "Vida"  && idade>10) {
+    console.log("Liberado");
+}
+
+let passaporte = true;
+
+if((nome == "Vida" && idade>30) || passaporte == true) {
+    console.log("Aprovado");
+}
