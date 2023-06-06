f3877527f7ed625abc0d8beae61334f9c94cef94
Vida Freitas <vidavalessa65@gmail.com>
2023-06-06 17:07:31 -0300

    feat; add 1 more lesson

---
+++ 2_program_structure/11_elso_if/script.js
@@ -0,0 +1,16 @@
+let nome = "Vida";
+let idade = 27;
+
+if(nome != undefined && nome == "lara") {
+    console.log("Nome está definido")
+} else if(nome == "Vida" && nome.lenght > 5 && idade == 50) {
+    console.log("O nome é Vida");
+} else {
+    console.log("Não é Vida");
+}
+
+if (1 > 2) {
+    console.log("Não");
+} else if (1 == 1) {
+    console.log("Sim")
+}
