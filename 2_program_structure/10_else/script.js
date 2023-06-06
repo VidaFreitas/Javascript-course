395d8ac9ace832d3b78054d0173ed10033ce5e9b
Vida Freitas <vidavalessa65@gmail.com>
2023-06-06 16:48:37 -0300

    feat; add 1 more lesson

---
+++ 2_program_structure/10_else/script.js
@@ -0,0 +1,15 @@
+let velocidade = 100;
+
+if(velocidade <=80) {
+    console.log("Não foi multado"); 
+} else {
+    console.log("Foi multado!");
+}
+
+let preço = 100;
+
+if(preço <= 80) {
+    console.log("Vou comprar")
+} else {
+    console.log("Não vou comprar")
+}
