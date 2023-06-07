f1e2692ddbb2811076167b4e30a444214b37a5ab
Vida Freitas <vidavalessa65@gmail.com>
2023-06-07 16:33:34 -0300

    feat; add lesson fuction

---
+++ 3_functions/1_define_fuction/script.js
@@ -0,0 +1,13 @@
+function imprimirNoConsole() {
+    console.log("Olá Mundo!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log("O número é: " + num);
+}
+
+imprimirUmNumero(4);
+imprimirUmNumero(5);
+imprimirUmNumero(6);
