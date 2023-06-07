0740afe6c2c87ff2c23198a2177a595d93df7247
Vida Freitas <vidavalessa65@gmail.com>
2023-06-07 16:01:10 -0300

    feat; add lesson switch

---
+++ 2_program_structure/17_switch/script.js
@@ -0,0 +1,13 @@
+ let nome = "Vida";
+
+ switch(nome) {
+    case "Vida":
+        console.log("O nome é Vida");
+        break;
+    case "Lara":
+        console.log("O nome é Lara");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+ }
