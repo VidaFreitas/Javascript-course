3a92e19e9f4e78e2ea2d0e3f327ca6927cf763ab
Vida Freitas <vidavalessa65@gmail.com>
2023-06-08 16:09:36 -0300

    feat; add lesson scope

---
+++ 3_functions/3_scope/script.js
@@ -0,0 +1,22 @@
+let y = 10;
+
+function imprimir() {
+    let y = 150;
+
+    console.log(y);
+}
+
+imprimir();
+
+console.log(y);
+
+
+let x = 10;
+
+if(x > 5) {
+    let x = 20;
+    x++;
+    console.log(x);
+}
+
+console.log(x);
