7ca57574e4f9322610621fe5090bfe13a852733c
Vida Freitas <vidavalessa65@gmail.com>
2023-06-13 16:43:38 -0300

    feat; add lesson arrays

---
+++ 4_arrays_objects/1_arrays/script.js
@@ -0,0 +1,15 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['Vida', 'João', "Aléxia"];
+let bool = [true, false, true];
+
+let mix = [1, 'Vida', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(mix);
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arr[arr.length - 1]);
