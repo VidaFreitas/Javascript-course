856405bb6cd722b7290bf53686549950b9ba79c3
Vida Freitas <vidavalessa65@gmail.com>
2023-06-14 16:20:40 -0300

    feat; add lesson objects

---
+++ 4_arrays_objects/4_objects/script.js
@@ -0,0 +1,23 @@
+let cachorro = {
+    patas: 4,
+    nome: 'Shark'
+};
+
+
+console.log(cachorro.patas);
+console.log(cachorro.nome);
+
+
+let pessoa = {
+    nome: 'Vida',
+    idade: 27,
+    profissão: "Product Manager"
+}
+
+console.log(pessoa.nome);
+
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+
+console.log(pessoa);
