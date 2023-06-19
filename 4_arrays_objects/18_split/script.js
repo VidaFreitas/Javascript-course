8690b9fac7ce20310b087f2ee3dfd9a25b07b4cf
Vida Freitas <vidavalessa65@gmail.com>
2023-06-19 16:22:55 -0300

    feat; add lesson split

---
+++ 4_arrays_objects/18_split/script.js
@@ -0,0 +1,11 @@
+let frase = "Testando o método o split";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let frutas = "Banana;Maçã;Jaca;Pera;Morango;Uva";
+
+console.log(frutas.split(';'));
