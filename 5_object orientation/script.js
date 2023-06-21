8ac02623e461d68eea1b0774b0703f64c41a479e
Vida Freitas <vidavalessa65@gmail.com>
2023-06-21 16:17:24 -0300

    feat; add lesson methods

---
+++ 5_object orientation/script.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    latir: function() {
+        console.log("Au Au Au");
+    },
+    rosnar: function() {
+        console.log("grrrr");
+    }
+}
+
+cachorro.latir();
+cachorro.rosnar();
