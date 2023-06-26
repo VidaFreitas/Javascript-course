7a97e58eed3e345f35149e4b8a8e0050b3890698
Vida Freitas <vidavalessa65@gmail.com>
2023-06-26 16:02:53 -0300

    feat; add lesson set and get

---
+++ 5_object orientation/13_get_and_set/script.js
@@ -0,0 +1,27 @@
+class Cachorro {
+    constructor(raca, cor) {
+        this.raca = raca;
+        this.cor = cor;
+    }
+
+    latir() {
+        console.log("Au Au");
+    }
+
+    get getCor() {
+        return this.cor;
+    }
+
+    set setCor(cor) {
+        this.cor = cor;
+    }
+}
+
+let pastor = new Cachorro('Pastor Alemão', 'Sem Cor');
+
+console.log(pastor);
+
+pastor.setCor = 'Marron';
+
+console.log(pastor.getCor);
+
