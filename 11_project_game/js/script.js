a4afa4639e7fd44147764f2bce49c5322c3013e9
Vida <vidavalessa65@gmail.com>
2023-08-15 17:10:54 +0200

    feat; add game

--- 11_project_game/js/script.js
+++ 11_project_game/js/script.js
@@ -1,9 +1,9 @@
-let x= document.querySelector(".x");
-let o= document.querySelector(".o");
-let boxes= document.querySelectorAll(".box");
-let buttons= document.querySelectorAll("#buttons-container button");
-let messageContainer= document.querySelector("#message");
-let messageText= document.querySelector("#message p");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
 let secondPlayer;
 
 // Moves Counter 
@@ -24,10 +24,38 @@ for(let i = 0; i < boxes.length; i++) {
 
             if(player1 == player2) {
                 player1++;
+
+                if(secondPlayer == `ai-player`) {
+
+                    computerPlay();
+
+                    player2++;
+                }
+
             } else {
                 player2++;
             }
+
+            checkWinCondition();
         }
+    });
+}
+
+for(let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function() {
+
+        secondPlayer = this.getAttribute("id");
+
+        for(let j = 0; j < buttons.length; i++) {
+            buttons[j].computedStyleMap.display = `none`;
+        }
+
+        setTimeout(function() {
+            let container = document.querySelector("#container")
+            container.classList.remove("hide");
+
+        }, 500);
     });
 }
 
@@ -40,4 +68,225 @@ function checkEl(player1, player2) {
     }
 
     return el;
+}
+
+function checkWinCondition() {
+
+    let b1 = document.getElementById("block-1");
+    let b2 = document.getElementById("block-2");
+    let b3 = document.getElementById("block-3");
+    let b4 = document.getElementById("block-4");
+    let b5 = document.getElementById("block-5");
+    let b6 = document.getElementById("block-6");
+    let b7 = document.getElementById("block-7");
+    let b8 = document.getElementById("block-8");
+    let b9 = document.getElementById("block-9");
+
+    if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length >0) {
+
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+
+        if(b1Child == `x` && b2Child == `x` && b3Child == `x`) {
+
+            DeclareWinner(`x`);
+
+        } else if(b1Child == `x` && b2Child == `x` && b3Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length >0) {
+
+        let b4Child = b4.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+
+        if(b4Child == `x` && b5Child == `x` && b6Child == `x`) {
+
+            DeclareWinner(`x`);
+
+        } else if(b4Child == `x` && b5Child == `x` && b6Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length >0) {
+
+        let b7Child = b7.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if(b7Child == `x` && b8Child == `x` && b9Child == `x`) {
+
+            DeclareWinner(`x`);
+            
+        } else if(b7Child == `x` && b8Child == `x` && b9Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length >0) {
+
+        let b1Child = b1.childNodes[0].className;
+        let b4Child = b4.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if(b1Child == `x` && b4Child == `x` && b7Child == `x`) {
+
+            DeclareWinner(`x`);
+            
+        } else if(b1Child == `x` && b4Child == `x` && b7Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length >0) {
+
+        let b2Child = b2.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+
+        if(b2Child == `x` && b5Child == `x` && b8Child == `x`) {
+
+            DeclareWinner(`x`);
+            
+        } else if(b2Child == `x` && b5Child == `x` && b8Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length >0) {
+
+        let b3Child = b3.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if(b3Child == `x` && b6Child == `x` && b9Child == `x`) {
+
+            DeclareWinner(`x`);
+            
+        } else if(b3Child == `x` && b6Child == `x` && b9Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length >0) {
+
+        let b1Child = b1.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+
+        if(b1Child == `x` && b5Child == `x` && b9Child == `x`) {
+
+            DeclareWinner(`x`);
+            
+        } else if(b1Child == `x` && b5Child == `x` && b9Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    if(b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length >0) {
+
+        let b3Child = b3.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if(b3Child == `x` && b5Child == `x` && b7Child == `x`) {
+
+            DeclareWinner(`x`);
+            
+        } else if(b3Child == `x` && b5Child == `x` && b7Child == `o`) {
+
+            DeclareWinner(`o`);
+        }
+
+    }
+
+    let Counter = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+        if(boxes[i].childNodes[0] != undefined) {
+            Counter++;
+        }
+    }
+
+    if(Counter == 9) {
+        DeclareWinner(`Deu Velha`);
+    }
+}
+
+function DeclareWinner(winner) {
+
+    let scoreboardX = document.querySelector("#scoreboard-1");
+    let scoreboardY = document.querySelector("#scoreboard-2");
+    let msg = ``;
+
+    if(winner == `x`) {
+        scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1;
+        msg = "O jogador Venceu!";
+    } else if(winner == `o`) {
+        scoreboardY.textContent = parseInt(scoreboardY.textContent) + 1;
+        msg = "O jogador 2 Venceu";
+    } else {
+        msg = "Deu velha!";
+    }
+
+    messageText.innerHTML = msg;
+    messageContainer.classList.remove("hide");
+
+
+    setTimeout(function() {
+        messageContainer.classList.add("hide");
+    }, 3000);
+
+    player1 = 0;
+    player2 = 0;
+
+    let boxesToRemove = document.querySelectorAll(".box div");
+
+    for(let i = 0; i < boxesToRemove.length; i++) {
+        boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+    }
+}
+
+function  computerPlay() {
+
+    let cloneO = o.cloneNode(true);
+    Counter = 0;
+    filled = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        if(boxes[i].childNodes[0] == undefined) {
+            if(randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                Counter++;
+                break;
+            }
+        } else {
+            filled++;
+        }
+    }
+
+    if(Counter == 0 && filled < 9) {
+        computerPlay();
+    }
 }
