fd9235477a61c4d0c53e490d9010328397db2dd9
Vida <vidavalessa65@gmail.com>
2023-08-22 16:56:29 +0200

    feat; add lesson write file

--- 12_node/1_project/index.js
+++ 12_node/1_project/index.js
@@ -1,9 +1,18 @@
-let {readFile} = require(`fs`);
+let {readFile, writeFile} = require(`fs`);
 
 readFile("arquivo.txt", "utf8", (error, texto) => {
     if(error) {
         throw error;
     } else {
         console.log(texto);
+    }
+});
+
+writeFile("arquivo.txt", "Texto por write file", (error) => {
+    
+    if(error) {
+        throw error;
+    } else {
+        console.log("Escrito com sucesso!");
     }
 });
